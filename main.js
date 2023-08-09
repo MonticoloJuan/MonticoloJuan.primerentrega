3b14c9662e029e24e2d7146b0bb7eba9b0a6d456
Monticolo Juan Ignacio <juan.monticolo@gmail.com>
2023-08-08 22:53:33 -0300

    v.2
    Corrección de función IF

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ alert("Bienvenido al gestionador de pagos en cuotas con tarjeta de crédito")
 do {
     let precioproducto = parseInt(prompt("Ingrese el Precio del Producto que quiera comprar"))
     let cuotas = parseInt(prompt("Ingrese la cantidad de cuotas que quiera realizar su pago (1-3-6-12)"))
-    if ((cuotas !== "") && (precioproducto !== "")) {
+    debugger
+    if ((cuotas > 0) && ( typeof cuotas === "number") && (precioproducto > 0) && (typeof precioproducto === "number")) {
         switch (cuotas) {
 
             case 1:
@@ -30,11 +31,10 @@ do {
                 alert("No ha ingresado un precio númerico o una cantidad de cuotas correctas")
                 break
         }
-        utilizar = prompt("Ingrese \"SI\", si quiere utilizar el calculador nuevamente o \"NO\", si quiere terminar te utilizarlo")
     } else {
         alert("No ha ingresado un precio númerico o una cantidad de cuotas correctas")
-        break
     }
+    utilizar = prompt("Ingrese \"SI\", si quiere utilizar el calculador nuevamente o \"NO\", si quiere terminar te utilizarlo")
 } while (utilizar !== "NO")
 
 /*
@@ -55,4 +55,4 @@ if (cuotas==1){
     precioproducto = precioproducto.toFixed(2)
     alert ("Ah elegido el pagar en 12 sola cuota con un 50% de interés, pagará un total de " + precioproducto)
 }
- */
+*/
